58b25fbc7dd8b66c5826f0414158acbae5fbc602
Faraz Patankar <farazpatankar@gmail.com>
2016-05-01 19:51:58 -0400

    Achieve basic functionality with some error handling and formatting

---
+++ lib/ruby_doc_parser.rb
@@ -0,0 +1,29 @@
+require 'open-uri'
+require 'nokogiri'
+class RubyDocParser
+	def initialize(search_string)
+		@search_string = search_string
+	end
+
+	def response
+		class_method = @search_string.gsub("?", "").split('#')
+		ruby_class, ruby_method = class_method[0], class_method[1]
+
+		doc = Nokogiri::HTML(open("http://ruby-doc.org/core-2.3.0/#{ruby_class.downcase.capitalize}.html"))
+		# binding.pry
+
+		container = ""
+
+		doc.css("##{ruby_method}-method").css("pre").css(".ruby").each do |tag|
+			container += tag.content
+		end
+
+		if container == ""
+			doc.css("div[id*=#{ruby_method}]").css("pre").css(".ruby").each do |tag|
+				container += tag.content
+			end
+		end
+
+		container.empty? ? "No examples for that method, you fuck :(" : container
+	end
+end
--- server.rb
+++ server.rb
@@ -1,5 +1,23 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require 'httparty'
+require 'sinatra/json'
 require 'json'
+require 'pry'
 
+require_relative("lib/ruby_doc_parser.rb")
+
+get '/' do
+	"Hi"
+end
+
+post '/docs' do
+	text = params["text"]
+	# binding.pry
+	if text.match(/[a-zA-Z]+([#]|[:]{2})[a-zA-Z_]+/)
+		ruby_doc_parser = RubyDocParser.new(text)
+		ruby_doc_parser.response
+	else
+		json :text => "Bro, you're doing it wrong. DAT attachment tho\n*Format : * `Class#method`\n*Example : * `String#reverse`"
+	end
+end
