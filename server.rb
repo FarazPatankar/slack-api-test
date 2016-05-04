2263b14f9b9f08517526dc5be2edf053c441e6d7
Faraz Patankar <farazpatankar@gmail.com>
2016-05-03 20:21:52 -0400

    Add link to response

--- server.rb
+++ server.rb
@@ -15,10 +15,25 @@ post '/docs' do
 	text = params["text"]
 	# binding.pry
 	if text.match(/[a-zA-Z]+([#]|[:]{2})[a-zA-Z_]+/)
+		class_method = text.gsub("?", "").split('#')
+		ruby_class, ruby_method = class_method[0], class_method[1]
+
 		ruby_doc_parser = RubyDocParser.new(text)
+
+
 		response = ruby_doc_parser.response
 		content_type :json
-		{ :response_type => 'in_channel', :text => response, :mrkdwn => true }.to_json
+		{
+			:response_type => 'in_channel',
+			:text => "Link : http://ruby-doc.org/core-2.2.0/#{ruby_class.capitalize}.html#method-i-#{ruby_method.downcase}",
+			:attachments => [
+				{
+					:text => "`#{response.gsub('"', "'")}`",
+					:mrkdwn_in => ["text"]
+				}
+			],
+			:mrkdwn => true
+		}.to_json
 	else
 		json :text => "Bro, you're doing it wrong. DAT attachment tho\n*Format : * `Class#method`\n*Example : * `String#reverse`"
 	end
